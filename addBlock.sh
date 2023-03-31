c4d4a9674229280cb65a0e4691b243e58d64af3a
Max Leibold <73217772+maxt3rbaxt3r@users.noreply.github.com>
2023-03-31 23:56:19 +0200

    Add files via upload

--- addBlock.sh
+++ addBlock.sh
@@ -7,4 +7,29 @@ sudo sed -i "$ a   type master;" named.conf.local
 sudo sed -i '$ a   file "/etc/bind/zones/block";' named.conf.local
 sudo sed -i "$ a };" named.conf.local
 
+sudo sed -i '$ a zone "welt.de" {' named.conf.local
+sudo sed -i "$ a   type master;" named.conf.local
+sudo sed -i '$ a   file "/etc/bind/zones/block";' named.conf.local
+sudo sed -i "$ a };" named.conf.local
+
+sudo sed -i '$ a zone "mobaplay.de" {' named.conf.local
+sudo sed -i "$ a   type master;" named.conf.local
+sudo sed -i '$ a   file "/etc/bind/zones/block";' named.conf.local
+sudo sed -i "$ a };" named.conf.local
+
+sudo sed -i '$ a zone "kinobay.de" {' named.conf.local
+sudo sed -i "$ a   type master;" named.conf.local
+sudo sed -i '$ a   file "/etc/bind/zones/block";' named.conf.local
+sudo sed -i "$ a };" named.conf.local
+
+sudo sed -i '$ a zone "researchopenworld.com" {' named.conf.local
+sudo sed -i "$ a   type master;" named.conf.local
+sudo sed -i '$ a   file "/etc/bind/zones/block";' named.conf.local
+sudo sed -i "$ a };" named.conf.local
+
+sudo sed -i '$ a zone "resjournals.com" {' named.conf.local
+sudo sed -i "$ a   type master;" named.conf.local
+sudo sed -i '$ a   file "/etc/bind/zones/block";' named.conf.local
+sudo sed -i "$ a };" named.conf.local
+
 sudo systemctl restart named
